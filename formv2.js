d31f89785e6d563f2b880da4e0446f6cd89c0305
faossb <143699741+faossb@users.noreply.github.com>
2023-09-09 16:33:48 +0530

    Create formv2.js

---
+++ formv2.js
@@ -0,0 +1,318 @@
+ const recruitmentData = 
+[
+  {
+    "category": "Navy",
+    "entries": "Indian Navy Entrance Test (INET)",
+    "qualification": "Graduation",
+    "stream": "Others",
+    "type": "Commission",
+    "percentage": "60",
+    "gender": "Male, Female",
+    "height_male": "157",
+    "height_female": "148",
+    "apply_month": "May,December",
+    "caste": "Open",
+    "age": "18-24"
+  },
+  {
+    "category": "Navy",
+    "entries": "Indian Naval Academy (INA)",
+    "qualification": "Graduation",
+    "stream": "BE, BTECH",
+    "type": "Commission",
+    "percentage": "",
+    "gender": "Male, Female",
+    "height_male": "157",
+    "height_female": "152",
+    "apply_month": "July,November",
+    "caste": "Open",
+    "age": "18-24"
+  },
+  {
+    "category": "Air-Force",
+    "entries": "Air Force Common Admission Test (AFCAT)",
+    "qualification": "Graduation",
+    "stream": "Others",
+    "type": "Commission",
+    "percentage": "60",
+    "gender": "Male, Female",
+    "height_male": "162",
+    "height_female": "162",
+    "apply_month": "July,December",
+    "caste": "Open",
+    "age": "18-25"
+  },
+  {
+    "category": "Air-Force",
+    "entries": "Air Force Academy (AFA)",
+    "qualification": "Graduation",
+    "stream": "BSC, BSCIT, BE, BTECH",
+    "type": "Commission",
+    "percentage": "",
+    "gender": "Male",
+    "height_male": "157",
+    "height_female": "",
+    "apply_month": "July,November",
+    "caste": "Open",
+    "age": "19-24"
+  },
+  {
+    "category": "Army",
+    "entries": "Officers Training Academy (OTA)",
+    "qualification": "Graduation",
+    "stream": "Others",
+    "type": "Commission",
+    "percentage": "",
+    "gender": "Male, Female",
+    "height_male": "157",
+    "height_female": "151",
+    "apply_month": "July,November",
+    "caste": "Open",
+    "age": "18-25"
+  },
+  {
+    "category": "Army",
+    "entries": "Territorial Army (TA)",
+    "qualification": "Graduation",
+    "stream": "BSC, BSCIT, BE, BTECH, Others",
+    "type": "Commission",
+    "percentage": "",
+    "gender": "Male,Female",
+    "height_male": "157",
+    "height_female": "151",
+    "apply_month": "June",
+    "caste": "Open",
+    "age": "18-42"
+  },
+  {
+    "category": "Army",
+    "entries": "Indian Military Academy (IMA)",
+    "qualification": "Graduation",
+    "stream": "Others",
+    "type": "Commission",
+    "percentage": "",
+    "gender": "Male",
+    "height_male": "157",
+    "height_female": "",
+    "apply_month": "July,November",
+    "caste": "Open",
+    "age": "18-24"
+  },
+  {
+    "category": "Para Military",
+    "entries": "Central Armed Police Forces (CAPF)",
+    "qualification": "Graduation",
+    "stream": "Others",
+    "type": "Commission",
+    "percentage": "",
+    "gender": "Male,Female",
+    "height_male": "157",
+    "height_female": "151",
+    "apply_month": "July,November",
+    "caste": "Open",
+    "age": "18-25"
+  },
+  {
+    "category": "Coast Guard",
+    "entries": "Assistant Commandant",
+    "qualification": "Graduation",
+    "stream": "Others",
+    "type": "Commission",
+    "percentage": "",
+    "gender": "Male,Female",
+    "height_male": "157",
+    "height_female": "151",
+    "apply_month": "July,November",
+    "caste": "Open",
+    "age": "18-25"
+  },
+  {
+    "category": "Air-Force",
+    "entries": "Short Service Commission (SSC)",
+    "qualification": "Graduation",
+    "stream": "BE, BTECH",
+    "type": "Commission",
+    "percentage": "60",
+    "gender": "Male",
+    "height_male": "162",
+    "height_female": "162",
+    "apply_month": "July,December",
+    "caste": "Open",
+    "age": "18-27"
+  },
+  {
+    "category": "Army",
+    "entries": "Technical Graduate Course (TGC)",
+    "qualification": "Graduation",
+    "stream": "BE, BTECH",
+    "type": "Commission",
+    "percentage": "60",
+    "gender": "Male",
+    "height_male": "157",
+    "height_female": "",
+    "apply_month": "June,November",
+    "caste": "Open",
+    "age": "18-27"
+  },
+  {
+    "category": "Navy",
+    "entries": "Indian Navy Entrance Test (INET)",
+    "qualification": "Graduation",
+    "stream": "BE, BTECH",
+    "type": "Commission",
+    "percentage": "60",
+    "gender": "Male",
+    "height_male": "157",
+    "height_female": "",
+    "apply_month": "Once a year",
+    "caste": "Open",
+    "age": "18-25"
+  },
+  {
+    "category": "Air-Force",
+    "entries": "AFCAT",
+    "qualification": "Graduation",
+    "stream": "BE, BTECH",
+    "type": "Commission",
+    "percentage": "60",
+    "gender": "Male,Female",
+    "height_male": "162",
+    "height_female": "162",
+    "apply_month": "December",
+    "caste": "Open",
+    "age": "20-24"
+  },
+  {
+    "category": "Air-Force",
+    "entries": "NDA (National Defence Academy)",
+    "qualification": "12th",
+    "stream": "Science (PCM)",
+    "type": "Commission",
+    "percentage": "45",
+    "gender": "Male,Female",
+    "height_male": "157",
+    "height_female": "157",
+    "apply_month": "June,December",
+    "caste": "Open",
+    "age": "16-19"
+  },
+  {
+    "category": "Air-Force (Flying Branch)",
+    "entries": "NDA (National Defence Academy)",
+    "qualification": "12th",
+    "stream": "Science (PCM)",
+    "type": "Commission",
+    "percentage": "45",
+    "gender": "Male,Female",
+    "height_male": "162.5",
+    "height_female": "163",
+    "apply_month": "June,December",
+    "caste": "Open",
+    "age": "16-19"
+  },
+  {
+    "category": "Indian Army",
+    "entries": "NDA (National Defence Academy)",
+    "qualification": "12th",
+    "stream": "Any Stream",
+    "type": "Commission",
+    "percentage": "50",
+    "gender": "Male,Female",
+    "height_male": "157 cm",
+    "height_female": "152 cm",
+    "apply_month": "April, October",
+    "caste": "Open",
+    "age": "16-19 years"
+  },
+  {
+    "category": "Indian Navy",
+    "entries": "NDA (National Defence Academy)",
+    "qualification": "12th",
+    "stream": "Science (PCM)",
+    "type": "Commission",
+    "percentage": "50",
+    "gender": "Male,Female",
+    "height_male": "165 cm",
+    "height_female": "157 cm",
+    "apply_month": "April, October",
+    "caste": "Open",
+    "age": "16-19 years"
+  },
+  {
+    "category": "Air-Force",
+    "entries": "Meteorology Entry",
+    "qualification": "Post Graduation",
+    "stream": "Meteorology",
+    "type": "Commission",
+    "percentage": "50",
+    "gender": "Male,Female",
+    "height_male": "157.5",
+    "height_female": "157.5",
+    "apply_month": "January,July",
+    "caste": "Open",
+    "age": "20-26"
+  }
+];
+
+   
+
+    let age;
+
+    document.getElementById('dob').addEventListener('change', function() {
+      const dob = new Date(this.value);
+      const currentDate = new Date();
+      age = currentDate.getFullYear() - dob.getFullYear();
+
+      if (age >= 16 && age < 50) {
+        document.getElementById('check-eligibility').disabled = false;
+      } else {
+        document.getElementById('check-eligibility').disabled = true;
+        alert('You must be at least 16 years old and less than 50 years old to apply.');
+      }
+    });
+
+    document.getElementById('qualification').addEventListener('change', function() {
+      const selectedValue = this.value;
+      const graduationOptions = document.getElementById('graduation-options');
+
+      if (selectedValue === 'graduation') {
+        graduationOptions.style.display = 'block';
+      } else {
+        graduationOptions.style.display = 'none';
+      }
+    });
+
+    document.getElementById('check-eligibility').addEventListener('click', function() {
+      const gender = document.getElementById('gender').value;
+      const qualification = document.getElementById('qualification').value;
+      const graduationStream = document.getElementById('graduation-stream').value;
+
+      const resultsContainer = document.getElementById('results-container');
+      resultsContainer.innerHTML = ''; // Clear previous results
+
+      for (const entry of recruitmentData) {
+        const entryAgeRange = entry.age.split('-');
+        const minAge = parseInt(entryAgeRange[0]);
+        const maxAge = parseInt(entryAgeRange[1]);
+
+        if (
+          age >= minAge && age <= maxAge &&
+          entry.gender.includes(gender) &&
+          ((qualification !== 'graduation' && entry.qualification === qualification) ||
+          (qualification === 'graduation' && entry.stream && entry.stream.split(', ').includes(graduationStream)))
+        ) {
+          const resultElement = document.createElement('div');
+          resultElement.classList.add('bubble');
+          const formattedData = Object.keys(entry).map(key => `${key}: ${entry[key]}`).join('<br>');
+          resultElement.innerHTML = formattedData;
+          resultsContainer.appendChild(resultElement);
+        }
+      }
+
+      if (resultsContainer.children.length === 0) {
+        const noResultsElement = document.createElement('div');
+        noResultsElement.classList.add('bubble');
+        noResultsElement.textContent = 'No matching categories found.';
+        resultsContainer.appendChild(noResultsElement);
+      }
+    });
